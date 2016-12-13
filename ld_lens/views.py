87a090bcae2c78916b47d3d6c617081ad758447d
Diarmuid Ryan <diarmuidr3d@gmail.com>
2016-12-13 23:00:36 +0000

    First basic view

--- ld_lens/views.py
+++ ld_lens/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from ld_lens.models import Person
 
 
@@ -8,4 +10,8 @@ def index(request):
 
 
 def person(request, person_id):
-    return HttpResponse("You have requested "+str(Person.objects.get(person_id=person_id).name))
+    template = loader.get_template('ld_lens/person.html')
+    context = {
+        'name': str(Person.objects.get(person_id=person_id).name),
+    }
+    return HttpResponse(template.render(context, request))
